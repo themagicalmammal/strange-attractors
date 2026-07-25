4714bb4a8575f7355fa58a1f536b8ce2116efaa2
Dipan Nanda <dipangithub@gmail.com>
2026-07-25 02:37:18 +0100

    fix: zoom out further on finance attractor

--- src/lib/systems.ts
+++ src/lib/systems.ts
@@ -480,7 +480,7 @@ export const systems: AttractorSystem[] = [
     equation: wrap(finance),
     id: "finance",
     initCoord: [0, -10, 0.1],
-    limits: { xlim: [-5, 5], ylim: [-16, -4], zlim: [-3, 3] },
+    limits: { xlim: [-8, 8], ylim: [-20, 0], zlim: [-5, 5] },
     name: "Finance",
     params: {
       defaults: [0.00001, 0.1, 1],
